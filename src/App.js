9c6980dbf261b602864bbaf6fd0afbb0c7356d69
Aravindan <aravindanravi33@gmail.com>
2020-07-01 19:13:13 +0530

    Made it ready for deployment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./containers/HomePage/index";
-import LoginForm from "./containers/LoginForm/index";
+// import LoginForm from "./containers/LoginForm/index";
 // import Tasks from "./components/Tasks/index";
 
 function App() {
   return (
     <React.Fragment>
       <Switch>
-        <Route exact path="/" component={LoginForm} />
-        <Route path="/tasks" component={HomePage} />
+        {/* <Route exact path="/" component={LoginForm} /> */}
+        <Route path="/" component={HomePage} />
       </Switch>
     </React.Fragment>
   );
